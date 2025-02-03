cb1763efdeffa91de80c2943daebcfce8e901278
lhhu <3479526453@qq.com>
2025-02-03 22:32:03 +0800

    完成图片管理页面

---
+++ src/pages/admin/PictureManagePage.vue
@@ -0,0 +1,167 @@
+<script setup lang="ts">
+import { onMounted, reactive, ref, computed } from 'vue'
+import { listUserVoByPageUsingPost, deleteUserUsingPost } from '@/api/userController.ts'
+import { message } from 'ant-design-vue'
+import dayjs from 'dayjs'
+
+const columns = [
+  {
+    title: 'id',
+    dataIndex: 'id',
+  },
+  {
+    title: '账号',
+    dataIndex: 'userAccount',
+  },
+  {
+    title: '用户名',
+    dataIndex: 'userName',
+  },
+  {
+    title: '头像',
+    dataIndex: 'userAvatar',
+  },
+  {
+    title: '简介',
+    dataIndex: 'userProfile',
+  },
+  {
+    title: '用户角色',
+    dataIndex: 'userRole',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+  },
+  {
+    title: '操作',
+    key: 'action',
+  },
+]
+
+// 定义数据
+const dataList = ref<API.UserVO>([])
+const total = ref(0)
+
+const fetchData = async () => {
+  const res = await listUserVoByPageUsingPost({ ...searchParams })
+
+  if (res.data.code === 0 && res.data.data) {
+    dataList.value = res.data.data.records ?? []
+    total.value = Number(res.data.data.total) ?? 0
+  } else {
+    message.error('数据获取失败：' + res.data.message)
+  }
+}
+
+// 钩子函数，页面加载时获取一次数据
+onMounted(() => {
+  fetchData()
+})
+
+//搜索条件
+const searchParams = reactive<API.UserQueryRequest>({
+  currentPage: 1,
+  pageSize: 10,
+  orderBy: 'createTime',
+  orderType: 'desc',
+})
+
+// 分页参数, 使用计算属性动态改变
+const pagination = computed(() => {
+  return {
+    current: searchParams.currentPage ?? 1,
+    pageSize: searchParams.pageSize ?? 10,
+    total: total.value ?? 0,
+    showSizeChanger: true,
+    showTotal: (total) => `共 ${total} 条`,
+  }
+})
+
+// 分页表格变化
+const doTableChange = (page: any) => {
+  searchParams.currentPage = page.current
+  searchParams.pageSize = page.pageSize
+  fetchData()
+}
+
+// 搜索数据
+const doSearch = () => {
+  // 重置页码
+  searchParams.currentPage = 1
+  fetchData()
+}
+
+// 删除数据
+const doDelete = async (id: string) => {
+  if (!id) {
+    return
+  }
+  const res = await deleteUserUsingPost({ id })
+  if (res.data.code === 0) {
+    message.success('删除成功')
+    // 刷新数据
+    await fetchData()
+  } else {
+    message.error('删除失败：' + res.data.message)
+  }
+}
+</script>
+
+<template>
+  <div id="userManagePage">
+    <a-form layout="inline" :model="searchParams" @finish="doSearch">
+      <a-form-item label="账号">
+        <a-input v-model:value="searchParams.userAccount" placeholder="输入账号" allow-clear />
+      </a-form-item>
+      <a-form-item label="昵称">
+        <a-input v-model:value="searchParams.userName" placeholder="输入昵称" allow-clear />
+      </a-form-item>
+      <a-form-item label="简介">
+        <a-textarea v-model:value="searchParams.userProfile" placeholder="输入简介" allow-clear />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">搜索</a-button>
+      </a-form-item>
+    </a-form>
+
+    <div style="margin-bottom: 10px" />
+
+    <a-table
+      :columns="columns"
+      :data-source="dataList"
+      :pagination="pagination"
+      @change="doTableChange"
+    >
+      <template #headerCell="{ column }" />
+
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'userAvatar'">
+          <a-image :width="30" :src="record.userAvatar" />
+        </template>
+
+        <template v-else-if="column.dataIndex === 'userRole'">
+          <div v-if="record.userRole === 'admin'">
+            <a-tag color="green">管理员</a-tag>
+          </div>
+          <div v-else>
+            <a-tag color="blue">普通用户</a-tag>
+          </div>
+        </template>
+
+        <template v-else-if="column.dataIndex === 'createTime'">
+          {{ dayjs(record.createTime).format('YYYY-MM-DD HH:mm:ss') }}
+        </template>
+
+        <template v-else-if="column.key === 'action'">
+          <a-button danger @click="doDelete(record.id)">删除</a-button>
+        </template>
+      </template>
+    </a-table>
+  </div>
+</template>
+
+<style scoped>
+#userManagePage {
+}
+</style>
